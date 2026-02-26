6fac02fe653f568649a22406d35a441d89320d3e
luisbader-source <luisbader@correo.unicordoba.edu.co>
2026-02-26 15:04:28 -0500

    actualizar jueves a 162 estudiantes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -86,7 +86,7 @@ const CONSOLIDATED_DATA = [
   { day: "Lunes", date: "23/02/2026", students: 103, completed: true },
   { day: "Martes", date: "24/02/2026", students: 332, completed: true },
   { day: "Miércoles", date: "25/02/2026", students: 0, completed: false },
-  { day: "Jueves", date: "26/02/2026", students: 150, completed: true },
+  { day: "Jueves", date: "26/02/2026", students: 162, completed: true },
   { day: "Viernes", date: "27/02/2026", students: 0, completed: false },
 ];
 
